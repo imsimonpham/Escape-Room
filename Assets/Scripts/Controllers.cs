14218ea9d29659972e5e9beae7af41aa265e82c0
simon <simonpham1294@gmail.com>
2023-12-03 21:30:55 -0400

    Added switch and lever triggers

---
+++ Assets/Scripts/Controllers.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Controllers : MonoBehaviour
+{
+   [SerializeField] private Animator _animator;
+
+   public void ToggleSwitch()
+   {
+      bool currentSwitchState = _animator.GetBool("SwitchOn");
+      _animator.SetBool("SwitchOn", !currentSwitchState);
+   }
+   
+   public void ToggleLever()
+   {
+      bool currentLeverState = _animator.GetBool("LeverOn");
+      _animator.SetBool("LeverOn", !currentLeverState);
+   }
+}
+                                                 
